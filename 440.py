71950d0aa28b555ed0865fb29d52e7e87414d5d4
allenyaoleetcode <allenyao07@yahoo.com>
2016-10-30 14:56:14 -0400

    440

--- 440.py
+++ 440.py
@@ -1,4 +1,4 @@
-440. K-th Smallest in Lexicographical Order
+# 440. K-th Smallest in Lexicographical Order
 
 class Solution(object):
     def findKthNumber(self, n, k):
@@ -7,20 +7,25 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        result =1
-        k-=1
-        while k>0:
-            count =0
-            interval=[result,result+1]
-            while interval[0]<=n:
-                count += min(n+1,interval[1])-interval[0]
-                interval=[interval[0]*10,interval[1]*10]
-            
-            if k>count:
-                result+=1
-                k-=count
+        result = 1
+        k -= 1
+        while k > 0:
+            count = 0
+            interval = [result, result + 1]
+            while interval[0] <= n:
+                count += min(n + 1, interval[1]) - interval[0]
+                interval = [interval[0] * 10, interval[1] * 10]
+
+            if k >= count:
+                result += 1
+                k -= count
             else:
-                result *=10
-                k-=1
-        
+                result *= 10
+                k -= 1
+
         return result
+
+
+a = Solution()
+
+print a.findKthNumber(2, 2)

54bfdcf086cb00d43bfbe1bf99f91ae070dcbedb
allenyaoleetcode <allenyao07@yahoo.com>
2016-10-30 14:43:06 -0400

    Create 440.py

---
+++ 440.py
@@ -0,0 +1,26 @@
+440. K-th Smallest in Lexicographical Order
+
+class Solution(object):
+    def findKthNumber(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        result =1
+        k-=1
+        while k>0:
+            count =0
+            interval=[result,result+1]
+            while interval[0]<=n:
+                count += min(n+1,interval[1])-interval[0]
+                interval=[interval[0]*10,interval[1]*10]
+            
+            if k>count:
+                result+=1
+                k-=count
+            else:
+                result *=10
+                k-=1
+        
+        return result
